8669b549a53407fa32635b534f8961892b892ab7
Pabullo-M <pabullo.muniz@gmail.com>
2024-04-29 22:00:15 -0300

    Exercicios js

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+function concatenarNomes(nome1, nome2){
+    let nomeCompleto = nome1 + " "+ nome2;
+    return nomeCompleto;
+}
+let nome1 = "Pabullo"
+let nome2 = "Motta"
+console.log(concatenarNomes(nome1, nome2));
